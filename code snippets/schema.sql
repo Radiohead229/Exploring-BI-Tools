1d76ba0d3f6b58725b7120d359725438b9a33f22
Raj Nayan Mir <rajnayanmir@gmail.com>
2024-10-25 19:30:42 +0530

    Create schema.sql

---
+++ code snippets/schema.sql
@@ -0,0 +1,90 @@
+SELECT * FROM crop.prod;
+alter table prod
+add column (start_date date, end_date date);
+-- adding two empty date columns
+
+UPDATE prod
+SET start_date = STR_TO_DATE(CONCAT(SUBSTRING_INDEX(year_range, '-', 1), '-01-01'), '%Y-%m-%d'),
+    end_date = STR_TO_DATE(CONCAT(SUBSTRING_INDEX(year_range, '-', 1), '-12-31'), '%Y-%m-%d');
+-- Splitting the column year_range into two separate date columns for precision in calculation
+
+select sum(Production)
+from prod;
+
+select distinct season
+from prod;
+select distinct crop
+from prod;
+-- to check the distinct seasons and crops
+
+alter table prod
+rename column Production to production;
+alter table prod
+rename column Year_Range to year_range;
+alter table prod
+rename column Crop to crop;
+-- changing the fonts to small letters for standardisation.
+
+select sum(production)
+from prod where start_date='2020-01-01';
+-- 
+
+
+delete from prod
+where crop = 'Jute ##'; 
+delete from prod
+where crop = 'Mesta ##';
+-- deleted the 2 inidividual rows for jute and mesta
+
+UPDATE prod
+SET production = production * 180 * 0.001
+WHERE crop = 'Jute & Mesta ##';
+-- calculation for converting lakh bales to tonnes
+UPDATE prod
+SET production = ROUND(production, 2);
+-- round the values in production column to 2 decimal places 
+
+update prod
+set crop = 'Jute and Mesta'
+where crop = 'Jute & Mesta ##';
+-- changed the names with hash in it for text clarity
+
+Select crop, season, year_range, production, start_date, end_date
+from prod
+where crop in ('Sugarcane','Cotton');
+--table showing for only sugarcane and cotton 
+
+delete from prod 
+where crop = 'Total Pulses';
+delete from prod
+where crop = 'Cereals';
+delete from prod 
+where crop = 'Total Food Grains';
+delete from prod
+where crop = 'Total Oil Seeds';
+-- deleting records for crops having the above names for deduplication
+
+
+select sum(production) as com_prod
+from prod where crop in ('Sugarcane','Cotton', 'Jute & Mesta');
+-- gives the total production of commercial crops
+
+select sum(production) as total
+from prod;
+-- gives the total production 
+
+select distinct crop
+from prod;
+
+select sum(production) as cotton
+from prod where crop = 'Cotton' and start_date ='2013-01-01';
+-- gives the total production of cotton in the year 2013
+
+
+SELECT crop, start_date, sum(production) as production
+FROM prod
+GROUP BY crop, start_date
+HAVING SUM(CASE WHEN season = 'rabi' THEN 1 ELSE 0 END) > 0
+AND SUM(CASE WHEN season = 'kharif' THEN 1 ELSE 0 END) = 0
+AND SUM(CASE WHEN season = 'summer' THEN 1 ELSE 0 END) = 0;
+-- finding the crops that grow only in a particular season and not the other(Season dependent crop)
